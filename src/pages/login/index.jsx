a0843e07ff9e03ab1d5b5da38c9f8c01933eabde
李轩逸 <1648525695@qq.com>
2022-09-20 10:51:23 +0800

    9.20am

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Form, Input, Button, Menu } from "antd";
 import "./index.css";
+import { loginAdmin } from "../../api/login";
 const Login = porps => {
   // state 声明
   const [selectdKey, setSelectdKey] = useState(["mail"]);
@@ -10,6 +11,27 @@ const Login = porps => {
     console.log("params:", params);
     setSelectdKey([params.key]);
   }
+  // function login() {
+  //   //登录
+  // }
+  function onFinish(values) {
+    //请求成功回调
+    console.log(111111);
+    console.log(values, "onFinsh");
+    const params={
+      username:values.phone,
+      userpass:values.password
+    }
+    loginAdmin(params).then(data=>{
+      console.log(data);
+    })
+  }
+  function onFinishFailed(values) {
+    //请求失败回调
+    // console.log(object);
+    console.log(values, "onFinishFailed");
+  }
+
   return (
     <div className="login">
       <div className="logincont">
@@ -27,19 +49,46 @@ const Login = porps => {
           className="login-form"
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
         >
           <div className="login-title">
             <h2>手机号登陆</h2>
           </div>
           {/* 手机号 */}
-          <Form.Item label="手机号">
+          <Form.Item
+            label="手机号"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                message: "请输入手机号",
+              },
+            ]}
+          >
             <Input placeholder="请输入手机号" />
           </Form.Item>
-          
-          <Form.Item label="密码">
+
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: "请输入密码" }]}
+          >
             <Input type="password" placeholder="请输入密码" />
           </Form.Item>
+       
+
+        <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
+          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+            登录
+          </Button>
+        </Form.Item>
         </Form>
+        {/* <div className="login-btn">
+          <Button type="primary" onClick={login}>
+            登录
+          </Button>
+        </div> */}
       </div>
     </div>
   );
